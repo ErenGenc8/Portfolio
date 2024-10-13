5fec0f807038b9fa47c58901e42234694e61de6b
ErenGenc8 <erengnc10@gmail.com>
2024-10-13 17:45:20 +0100

    Create registerfile.v

---
+++ Verilog Projects/Neural Network Processor/registerfile.v
@@ -0,0 +1,21 @@
+module RegisterFile(
+    input clk,
+    input [4:0] read_addr1, read_addr2, write_addr,
+    input [31:0] write_data,
+    input reg_write,
+    output reg [31:0] read_data1, read_data2
+);
+    reg [31:0] registers [0:31];
+
+    always @(posedge clk) begin
+        if (reg_write) begin
+            registers[write_addr] <= write_data;
+        end
+    end
+
+    always @(*) begin
+        read_data1 = registers[read_addr1];
+        read_data2 = registers[read_addr2];
+    end
+endmodule
+
